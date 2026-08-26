de7991cde415cef24d08df713d404cb8dc24f220
Harsh830 <harshamittiwari1@gmail.com>
2026-08-26 19:19:05 +0530

    Add Firebase configuration to firebase.js

---
+++ frontend/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBqMg4dMA8ZTTM3_kudC8yZH1auA5ix9sA",
+  authDomain: "globaltalk-2bde5.firebaseapp.com",
+  projectId: "globaltalk-2bde5",
+  storageBucket: "globaltalk-2bde5.firebasestorage.app",
+  messagingSenderId: "48446575460",
+  appId: "1:48446575460:web:8d7f423049cc80aa07a071",
+  measurementId: "G-XMVFH6DNEQ"
+};
