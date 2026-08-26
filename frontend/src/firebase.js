2062efa71d19e84f1f9a5aa5c4fb7bf58408c3df
Harsh830 <harshamittiwari1@gmail.com>
2026-08-26 19:37:21 +0530

    Add Firebase configuration to firebase.js
    Added Firebase configuration details to firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,7 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // YAHAN APNA WAHI FIREBASE CONFIG RAKHO
+  apiKey: "AIzaSyBqMg4dMA8ZTTM3_kudC8yZH1auA5ix9sA",
+  authDomain: "globaltalk-2bde5.firebaseapp.com",
+  projectId: "globaltalk-2bde5",
+  storageBucket: "globaltalk-2bde5.firebasestorage.app",
+  messagingSenderId: "48446575460",
+  appId: "1:48446575460:web:8d7f423049cc80aa07a071",
+  measurementId: "G-XMVFH6DNEQ"
 };
 
 const app = initializeApp(firebaseConfig);
